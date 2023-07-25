a0f3583f61103b8eacf4b72f9c6d22dd13339d40
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-07-25 18:46:42 +0700

    Update configFile on AWS

--- BackEnd/core/settings.py
+++ BackEnd/core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-&5^0nz@l+d-*2m)m^$m!g-1xu#*kcahlk%3riryn&ocyg=&%gg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
