79887351f360f93351c1ef3a47296318f0bc8060
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-07-25 17:12:34 +0700

    Deploy BE on EC2 AWS

--- BackEnd/core/settings.py
+++ BackEnd/core/settings.py
@@ -128,14 +128,14 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://localhost:8000",
-    "http://127.0.0.1:3000",
-    "http://127.0.0.1:8000",
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:3000",
+#     "http://localhost:8000",
+#     "http://127.0.0.1:3000",
+#     "http://127.0.0.1:8000",
 
-]
-# CORS_ORIGIN_ALLOW_ALL = False
+# ]
+CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ORIGIN_WHITELIST = [
 #     "https://example.com",
 #     "https://sub.example.com",
