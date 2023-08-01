5ade7a5d313eabed8e6e0027ad3aec9b00f8285b
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-08-01 15:38:09 +0700

    Update FE

--- BackEnd/core/settings.py
+++ BackEnd/core/settings.py
@@ -196,7 +196,7 @@ AUTH_USER_MODEL = "users.NewUser"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
--- FrontEnd/blogapi/src/components/posts/single.js
+++ FrontEnd/blogapi/src/components/posts/single.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axiosInstance from '../../axios';
 import { useParams } from 'react-router-dom';
-//MaterialUI
+// MaterialUI
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
@@ -9,28 +9,81 @@ import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Avatar from '@material-ui/core/Avatar';
-import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import CommentIcon from '@material-ui/icons/Comment';
+import VisibilityIcon from '@material-ui/icons/Visibility';
+import TextField from '@material-ui/core/TextField'; // Import TextField from @material-ui/core
+import Button from '@material-ui/core/Button'; // Import Button from @material-ui/core
+
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        marginTop: theme.spacing(8),
+    heroContent: {
+        padding: theme.spacing(8, 0, 6),
+    },
+    postImage: {
+        width: '100%',
+        marginBottom: theme.spacing(2),
+    },
+    postTitle: {
+        marginBottom: theme.spacing(2),
+    },
+    postExcerpt: {
+        marginBottom: theme.spacing(2),
+    },
+    postContent: {
+        textAlign: 'justify',
+        whiteSpace: 'pre-wrap',
+        marginBottom: theme.spacing(2),
+        fontFamily: 'cursive',
+    },
+    authorInfo: {
         display: 'flex',
-        flexDirection: 'column',
         alignItems: 'center',
-        fontFamily: 'cursive', // Thay đổi font chữ sang cursive
+        marginBottom: theme.spacing(2),
+    },
+    authorAvatar: {
+        marginRight: theme.spacing(2),
+    },
+    viewLikeComment: {
+        display: 'flex',
+        alignItems: 'center',
+        marginBottom: theme.spacing(2),
+    },
+    viewLikeCommentIcon: {
+        marginRight: theme.spacing(1),
     },
     comment: {
-        marginTop: theme.spacing(2),
         padding: theme.spacing(2),
         backgroundColor: '#f5f5f5',
         borderRadius: theme.spacing(1),
-        fontFamily: 'cursive', // Thay đổi font chữ sang cursive
+        marginBottom: theme.spacing(2),
+        fontFamily: 'cursive',
     },
-    postContent: {
-        textAlign: 'justify', // Căn đều hai lề
-        whiteSpace: 'pre-wrap', // Xuống dòng giống như trong trang admin
-        marginBottom: theme.spacing(2), // Khoảng cách dưới nội dung
-        fontFamily: 'cursive', // Thay đổi font chữ sang cursive
+    commentSection: {
+        marginTop: theme.spacing(4),
+        padding: theme.spacing(2),
+        backgroundColor: '#f0f0f0',
+        borderRadius: theme.spacing(1),
+    },
+    commentForm: {
+        marginTop: theme.spacing(2),
+    },
+    commentInput: {
+        width: '100%',
+    },
+    commentButton: {
+        marginLeft: theme.spacing(2),
+    },
+    commentAvatar: {
+        marginRight: theme.spacing(2),
+    },
+    commentContent: {
+        textAlign: 'left',
+    },
+    commentCreatedAt: {
+        marginTop: theme.spacing(1),
+        color: theme.palette.text.secondary,
     },
 }));
 
@@ -40,83 +93,162 @@ export default function Post() {
     const classes = useStyles();
 
     const [data, setData] = useState({
-        posts: [],
+        post: [],
+        newComment: "",
     });
 
     useEffect(() => {
-        axiosInstance.get('post/' + slug + '/').then((res) => {
-            setData({
-                posts: res.data,
+        axiosInstance.get('post/' + slug + '/')
+            .then((response) => {
+                setData({
+                    post: response.data,
+                    newComment: "",
+                });
             });
-            console.log(res.data);
-        });
-    }, [setData]);
+    }, [slug]);
+
+    const handleCommentChange = (e) => {
+        setData((prevData) => ({
+            ...prevData,
+            newComment: e.target.value,
+        }));
+    };
+
+    const handleAddComment = () => {
+        // TODO: Gửi yêu cầu API để thêm comment vào bài viết
+        // Sau khi thêm thành công, cập nhật state để hiển thị comment mới
+        setData((prevData) => ({
+            ...prevData,
+            post: {
+                ...prevData.post,
+                comments: [
+                    {
+                        id: Date.now(),
+                        author: { user_name: "User", avatar: "" }, // Thay bằng thông tin người dùng thực tế
+                        content: prevData.newComment,
+                        created_at: new Date().toISOString(),
+                    },
+                    ...prevData.post.comments,
+                ],
+            },
+            newComment: "",
+        }));
+    };
 
     return (
         <Container component="main" maxWidth="xl">
             <CssBaseline />
-            <div className={classes.paper}>
-                <div className={classes.heroContent}>
-                    <Container maxWidth="xl">
-                        {/* Thêm hình ảnh vào đầu bài viết */}
-                        <img src={data.posts.image} alt={data.posts.title} style={{ width: '100%', marginBottom: 10 }} />
+            <div className={classes.heroContent}>
+                <img
+                    src={data.post.image}
+                    alt={data.post.title}
+                    className={classes.postImage}
+                />
+                <Typography
+                    variant="h2"
+                    align="center"
+                    color="textPrimary"
+                    gutterBottom
+                    className={classes.postTitle}
+                >
+                    {data.post.title}
+                </Typography>
+                <Typography
+                    variant="h5"
+                    align="center"
+                    color="textSecondary"
+                    paragraph
+                    className={classes.postExcerpt}
+                >
+                    {data.post.excerpt}
+                </Typography>
+                <Typography
+                    variant="body1"
+                    align="justify"
+                    color="textSecondary"
+                    paragraph
+                    className={classes.postContent}
+                >
+                    {data.post.content}
+                </Typography>
 
-                        <Typography
-                            component="h1"
-                            variant="h2"
-                            align="center"
-                            color="textPrimary"
-                            gutterBottom
-                        >
-                            {data.posts.title}
-                        </Typography>
-                        <Typography
-                            variant="h5"
-                            align="center"
-                            color="textSecondary"
-                            paragraph
-                        >
-                            {data.posts.excerpt}
-                        </Typography>
-                        <Typography
-                            variant="h5"
-                            align="center"
-                            color="textSecondary"
-                            paragraph
-                            className={classes.postContent} // Thêm lớp CSS tùy chỉnh vào đây
-                        >
-                            {data.posts.content}
-                        </Typography>
-                    </Container>
-                </div>
-                {data.posts.comments && data.posts.comments.length > 0 && (
-                    <div>
-                        <Typography variant="h5" gutterBottom>
-                            Comments
-                        </Typography>
-                        {data.posts.comments.map((comment) => (
-                            <Paper className={classes.comment} key={comment.id}>
-                                <Grid container wrap="nowrap" spacing={2}>
-                                    <Grid item>
-                                        <Avatar alt={comment.author.username} src={MEDIA_URL + comment.author.avatar} />
-                                    </Grid>
-                                    <Grid justifyContent="left" item xs zeroMinWidth>
+                <Grid container spacing={2} alignItems="center" justify="space-between">
+                    {/* ... (your existing code) */}
+                </Grid>
+            </div>
 
-                                        <Typography variant="h6" style={{ margin: 0, textAlign: "left" }}>
-                                            {comment.author.user_name}
-                                        </Typography>
-                                        <Typography variant="body1" style={{ textAlign: "left" }}>
-                                            {comment.content}
-                                        </Typography>
-                                        <Typography variant="body2" style={{ textAlign: "left", color: "gray" }}>
-                                            created at {(comment.created_at)}
-                                        </Typography>
-                                    </Grid>
+            {/* Khu vực comment */}
+            <div className={classes.commentSection}>
+                <Typography variant="h5" gutterBottom>
+                    Comments
+                </Typography>
+                {/* Form nhập comment */}
+                <Grid container spacing={2} alignItems="center" className={classes.commentForm}>
+                    <Grid item xs={12} sm={2}>
+                        {data.post.author && (
+                            <>
+                                <Avatar
+                                    alt={data.post.author.user_name}
+                                    src={MEDIA_URL + data.post.author.avatar}
+                                    className={classes.authorAvatar}
+                                />
+                                <Typography variant="body1">
+                                    {data.post.author.user_name}
+                                </Typography>
+                            </>
+                        )}
+                    </Grid>
+                    <Grid item xs={12} sm={7}>
+                        <TextField
+                            variant="outlined"
+                            fullWidth
+                            multiline
+                            rows={3}
+                            value={data.newComment}
+                            onChange={handleCommentChange}
+                            placeholder="Write a comment..."
+                            className={classes.commentInput} // Apply the custom class for comment input
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={3}>
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            fullWidth
+                            onClick={handleAddComment}
+                            className={classes.commentButton} // Apply the custom class for comment button
+                        >
+                            Add Comment
+                        </Button>
+                    </Grid>
+                </Grid>
+
+                {/* Các ô comment */}
+                {data.post.comments &&
+                    data.post.comments.map((comment) => (
+                        <Paper className={classes.comment} key={comment.id}>
+                            <Grid container wrap="nowrap" spacing={2} alignItems="center">
+                                <Grid item>
+                                    <Avatar
+                                        alt={comment.author.user_name}
+                                        src={MEDIA_URL + comment.author.avatar}
+                                        className={classes.commentAvatar}
+                                    />
                                 </Grid>
-                            </Paper>
-                        ))}
-                    </div>
-                )}
+                                <Grid item xs zeroMinWidth>
+                                    <Typography variant="h6" className={classes.commentContent}>
+                                        {comment.author.user_name}
+                                    </Typography>
+                                    <Typography variant="body1" className={classes.commentContent}>
+                                        {comment.content}
+                                    </Typography>
+                                    <Typography variant="body2" className={classes.commentCreatedAt}>
+                                        Created at {comment.created_at}
+                                    </Typography>
+                                </Grid>
+                            </Grid>
+                        </Paper>
+                    ))}
             </div>
         </Container>
     );
