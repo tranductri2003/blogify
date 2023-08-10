458cefa28d6c3b258d8b86e399517bcc8bb67b2b
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-08-10 17:27:48 +0700

    Update Static Files

--- BackEnd/core/settings.py
+++ BackEnd/core/settings.py
@@ -141,8 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+
 
 # CORS_ALLOWED_ORIGINS = [
 #     "http://localhost:3000",
@@ -248,6 +247,9 @@ SIMPLE_JWT = {
     "SLIDING_TOKEN_OBTAIN_SERIALIZER": "rest_framework_simplejwt.serializers.TokenObtainSlidingSerializer",
     "SLIDING_TOKEN_REFRESH_SERIALIZER": "rest_framework_simplejwt.serializers.TokenRefreshSlidingSerializer",
 }
+
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = 'media/'
