f4be8168b16ae2c7e6393adcf5bfe1c3f736993e
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-08-11 12:04:57 +0700

    Update admin site

--- BackEnd/chat/admin.py
+++ BackEnd/chat/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Room,Message
+from .models import Room, Message
 
-admin.site.register(Room)
-admin.site.register(Message)
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'description') 
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'room',  'content', 'created_on')
