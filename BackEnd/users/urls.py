7fc86535fa16c0006ca13ddc22fbbfa8deeb4232
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-08-11 17:11:33 +0700

    Update URL user (method not allowed) and config file

--- BackEnd/users/urls.py
+++ BackEnd/users/urls.py
@@ -6,11 +6,12 @@ app_name = 'users'
 
 urlpatterns = [
     path('edit/<str:user_name>/', EditUser.as_view(), name='edituser'),
-    path('', UserList.as_view(), name='listusers'),
     path('register/', CustomUserCreate.as_view(), name="create_user"),
     path('logout/blacklist/', BlacklistTokenUpdateView.as_view(),
          name='blacklist'),
     path('<str:user_name>/', UserDetail.as_view(), name='detailuser'),
+    path('', UserList.as_view(), name='listusers'),
+
     # path('<str:user_name>/updateview/<int:num>/',
     #      UpdateView.as_view(), name='updateuserview'),
     # path('<str:user_name>/updatelike/<int:num>/',
